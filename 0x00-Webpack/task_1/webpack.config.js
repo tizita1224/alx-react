5e528ff032aab7c8176a66aa9bc24f7cce48a6d9
kase2228 <tizitabelachew80@gmail.com>
2024-11-23 02:52:59 +0300

    files

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,17 +1,19 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
-  entry: './js/dashboard_main.js', 
-    filename: 'bundle.js', 
-    path: path.resolve(__dirname, 'public'), 
+  mode: 'production', // Mode for Webpack
+  entry: './js/dashboard_main.js', // Entry point of your application
+  output: {
+    filename: 'bundle.js', // Name of the output file
+    path: path.resolve(__dirname, 'public'), // Output directory
+  },
   module: {
     rules: [
       {
-        test: /\.js$/, 
-        exclude: /node_modules/,
+        test: /\.js$/, // Matches JavaScript files
+        exclude: /node_modules/, // Excludes node_modules from processing
         use: {
-          loader: 'babel-loader', 
+          loader: 'babel-loader', // If using Babel for transpiling (optional)
         },
       },
     ],
